860416a139978ad047474a747b606beb3e4983a2
shafik720 <shafikrasel5@gmail.com>
2022-12-26 22:37:25 +0600

    working on js

--- app2.js
+++ app2.js
@@ -3,6 +3,9 @@ let uploadSection = document.querySelector('.upload-section'),
 uploadImg = uploadSection.querySelector('img'),
 uploadInput = uploadSection.querySelector('input');
 
+let widthValue = document.querySelector('.width');
+let heightValue = document.querySelector('.height');
+
 function upload(e){
     let file = e.target.files[0];
     if(!file){
@@ -11,6 +14,8 @@ function upload(e){
     uploadImg.src = URL.createObjectURL(file);
     uploadImg.addEventListener('load',()=>{
         uploadSection.classList.add('active');
+        widthValue.value = uploadImg.naturalWidth;
+        heightValue.value = uploadImg.naturalHeight;
     })
     
 }
