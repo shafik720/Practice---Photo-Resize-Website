e180a2891125659434429538cb268491f6c388cd
shafik720 <shafikrasel5@gmail.com>
2022-12-26 20:37:25 +0600

    working on js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 
 const uploadBox = document.querySelector('.upload-section'),
+previewImg = uploadBox.querySelector('img'),
 inputFile = uploadBox.querySelector('input');
 
+function loadFile(e){
+    let file = e.target.files[0];
+    if(!file){
+        return;
+    }
+    previewImg.src = URL.createObjectURL(file);
+    uploadBox.classList.add('active');
+    console.log(file);
+}
+
+inputFile.addEventListener('change', loadFile);
 uploadBox.addEventListener('click', ()=>{inputFile.click()});
