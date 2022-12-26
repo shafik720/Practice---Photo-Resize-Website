36ecad1711511eda821fa16a10e65d657fb2f214
shafik720 <shafikrasel5@gmail.com>
2022-12-26 20:48:36 +0600

    working on js

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const uploadBox = document.querySelector('.upload-section'),
 previewImg = uploadBox.querySelector('img'),
 inputFile = uploadBox.querySelector('input');
 
+let widthValue = document.querySelector('.width');
+let heightValue = document.querySelector('.height');
+
 function loadFile(e){
     let file = e.target.files[0];
     if(!file){
         return;
     }
     previewImg.src = URL.createObjectURL(file);
+    
     uploadBox.classList.add('active');
     console.log(file);
 }
