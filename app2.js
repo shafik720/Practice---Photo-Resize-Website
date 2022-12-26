a40476755559a876bfd7bb6bfcc63c15961f81b5
shafik720 <shafikrasel5@gmail.com>
2022-12-26 22:33:42 +0600

    working on js

--- app2.js
+++ app2.js
@@ -1,5 +1,19 @@
 
 let uploadSection = document.querySelector('.upload-section'),
+uploadImg = uploadSection.querySelector('img'),
 uploadInput = uploadSection.querySelector('input');
 
+function upload(e){
+    let file = e.target.files[0];
+    if(!file){
+        return;
+    }
+    uploadImg.src = URL.createObjectURL(file);
+    uploadImg.addEventListener('load',()=>{
+        uploadSection.classList.add('active');
+    })
+    
+}
+
+uploadInput.addEventListener('change',upload);
 uploadSection.addEventListener('click',()=>uploadInput.click());
