4cdedc4b5b810b1a1c6c961970f9ff13c9663efa
shafik720 <shafikrasel5@gmail.com>
2022-12-27 18:17:22 +0600

    working on second js file

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ inputFile = uploadBox.querySelector('input');
 let widthValue = document.querySelector('.width');
 let heightValue = document.querySelector('.height');
 
+let ratioButton = document.querySelector('#ratioCheckbox');
+
 function loadFile(e){
     let file = e.target.files[0];
     if(!file){
@@ -19,6 +21,12 @@ function loadFile(e){
         heightValue.value = previewImg.naturalHeight;
     })    
 }
+function changeHeight(){
+    let height = ratioButton.checked ? 500 : 0;
+
+    heightValue.value = height;
+}
+widthValue.addEventListener('keyup',changeHeight);
 
 inputFile.addEventListener('change', loadFile);
 uploadBox.addEventListener('click', ()=>{inputFile.click()});
